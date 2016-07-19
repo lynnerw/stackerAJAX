f89fcc2dcea0cead88dfb882565190a2de93aab5
lynnerw <larwitter@gmail.com>
2016-07-19 17:01:59 -0400

    started proccess of adding functionality for get top answerer

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 // this function takes the question object returned by the StackOverflow request
 // and returns new result to be appended to DOM
 var showQuestion = function(question) {
-	
+
 	// clone our result template code
 	var result = $('.templates .question').clone();
-	
+
 	// Set the question properties in result
 	var questionElem = result.find('.question-text a');
 	questionElem.attr('href', question.link);
@@ -32,6 +32,39 @@ var showQuestion = function(question) {
 };
 
 
+// this function takes the tag score object returned by the StackOverflow request
+// and returns new result to be appended to DOM
+var showAnswerer = function(answerer) {
+
+	// clone our result template code
+	var result = $('.templates .answerer').clone();
+
+	// set the user's display name in result
+	var asked = result.find('.display-name');
+	var date = new Date(1000*question.creation_date);
+	asked.text(date.toString());
+
+	// set the accept rate for the user for this tag in result
+	var accepted = result.find('.accept-rate');
+	viewed.text(question.view_count);
+
+	// set the user's score
+	var asker = result.find('.score');
+	asker.html('<p>Name: <a target="_blank" '+
+		'href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+		question.owner.display_name +
+		'</a></p>' +
+		'<p>Reputation: ' + question.owner.reputation + '</p>'
+	);
+
+	// set the answerer's user link in result
+	var answererLink = result.find('.link a');
+	answererLink.attr???('href', answerer.link);
+	answererLink.text???(answerer.title???);
+
+	return result;
+};
+
 // this function takes the results object from StackOverflow
 // and returns the number of results and tags to be appended to DOM
 var showSearchResults = function(query, resultNum) {
@@ -49,15 +82,15 @@ var showError = function(error){
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
 var getUnanswered = function(tags) {
-	
+
 	// the parameters we need to pass in our request to StackOverflow's API
-	var request = { 
+	var request = {
 		tagged: tags,
 		site: 'stackoverflow',
 		order: 'desc',
 		sort: 'creation'
 	};
-	
+
 	$.ajax({
 		url: "http://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
@@ -81,6 +114,47 @@ var getUnanswered = function(tags) {
 	});
 };
 
+// passes a tag string to search for top answerers
+// on this topic on StackOverflow
+var getAnswerer = function(tag) {
+
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = {
+		"page":"number",
+		"pagesize":"number",
+		"tag":"string",
+		"period":["all_time","month"]};
+
+	var method = "/2.2/tags/{tag}/top-answerers/{period}";
+
+		tagged: tag,
+		site: 'stackoverflow',
+		order: 'desc',
+		sort: 'creation'
+	};
+
+	$.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/" ,
+		data: request,
+		dataType: "jsonp",//use jsonp to avoid cross origin issues
+		type: "GET",
+	})
+	.done(function(result){ //this waits for the ajax to return with a succesful promise object
+		var searchResults = showSearchResults(request.tag, result.items.length);
+
+		$('.search-results').html(searchResults);
+		//$.each is a higher order function. It takes an array and a function as an argument.
+		//The function is executed once for each item in the array.
+		$.each(result.items, function(i, item) {
+			var question = showQuestion(item);
+			$('.results').append(question);
+		});
+	})
+	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
 
 $(document).ready( function() {
 	$('.unanswered-getter').submit( function(e){
@@ -90,5 +164,13 @@ $(document).ready( function() {
 		// get the value of the tags the user submitted
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
+	});
+	$('.inspiration-getter').submit( function(e){
+		e.preventDefault();
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the top 30 answerers of questions with the tags the user submitted
+		var tags = $(this).find("input[name='tags']").val();
+		getAnswerer(tags);
 	});
 });
